4c8037a5dbf2f98f0aa438e4a973fd4f5a13fea5
lazarowend <lazaroclans1@gmail.com>
2024-12-02 20:54:58 -0300

    init templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def login():
-    return 'teste'
+
+def login(request):
+    return render(request, 'accounts/login.html')
