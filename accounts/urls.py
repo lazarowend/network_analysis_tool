02f552ac7caaeac171e6fa6850194a26ba55d7b0
lazarowend <lazaroclans1@gmail.com>
2024-12-02 23:46:46 -0300

    add template login and register

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from accounts.views import login
+from accounts.views import login, register
 
 
 urlpatterns = [
     path('accounts/login/', login, name='login'),
+    path('accounts/register/', register, name='register'),
 ]
